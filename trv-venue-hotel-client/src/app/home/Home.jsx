c5c2576ada8eedab20eaaff61be37cc904b580dd
Jorge Luis Beroisa <jberoisa@gmail.com>
2018-12-03 10:34:00 -0300

    changed functional component to class and added axios

--- trv-venue-hotel-client/src/app/home/Home.jsx
+++ trv-venue-hotel-client/src/app/home/Home.jsx
@@ -1,9 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-export default function Home(){
-	return (
-		<Fragment>
-			Home
-		</Fragment>
-	)
+const Container = styled.header`
+	background-color: #37454d;
+	height: 100vh;
+	margin: auto;
+	border: 1px solid #c3c3c3;
+	border-width: 0 1px 0 1px;
+`;
+
+export default class Home extends Component {
+	state = {
+		hotels: [],
+	}
+	componentDidMount = () => {
+		axios.get('/hotels')
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+			.then(function () {
+			});
+	}
+	render = () => {
+		return (
+			<Container>
+				Home
+			</Container>
+		)
+	}
 }
